bc693db5acaf4196c1978da13723853dd169e311
jonah ominyi <jonahjohnzon@gmail.com>
2024-05-30 13:32:45 +0000

    user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -51,22 +51,25 @@ class DB:
             raise
         return new_user
 
-    def find_user_by(self, **kwargs) -> User:
-        """Finds a user based on a set of filters.
+    def find_user_by(self, **kwargs: Dict[str, str]) -> User:
+        """Find user by specified attributes.
+
+        Raises:
+            error: NoResultFound: When no results found.
+            error: InvalidRequestError: When invalid query arguments are passed
+
+        Returns:
+            User: First row found `users` table.
         """
-        fields, values = [], []
-        for key, value in kwargs.items():
-            if hasattr(User, key):
-                fields.append(getattr(User, key))
-                values.append(value)
-            else:
-                raise InvalidRequestError()
-        result = self._session.query(User).filter(
-            tuple_(*fields).in_([tuple(values)])
-        ).first()
-        if result is None:
+        session = self._session
+        try:
+            user = session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
             raise NoResultFound()
-        return result
+        except InvalidRequestError:
+            raise InvalidRequestError()
+        # print("Type of user: {}".format(type(user)))
+        return user
 
     def update_user(self, user_id: int, **kwargs) -> None:
         """Updates user's attributes user ID and arbitrary keyword
